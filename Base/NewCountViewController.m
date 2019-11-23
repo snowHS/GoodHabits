286b46906f4ec9da39a847d76cef5d1b6dcb3d3c
snowHS <57667157+snowHS@users.noreply.github.com>
2019-11-23 10:05:36 +0800

    no message

--- Base/NewCountViewController.m
+++ Base/NewCountViewController.m
@@ -9,7 +9,9 @@
 #import "NewCountViewController.h"
 
 @interface NewCountViewController ()
-
+{
+    UITextField * countTf;
+}
 @end
 
 @implementation NewCountViewController
@@ -55,25 +57,101 @@
         make.height.mas_equalTo(44);
     }];
     
-    UITextField * nametf = [[UITextField alloc] init];
-    nametf.font = [UIFont systemFontOfSize:15];
-    nametf.textColor = BlackColor;
-    nametf.placeholder = @"请输入计数的标题";
-    [view addSubview:nametf];
-    [nametf mas_makeConstraints:^(MASConstraintMaker *make) {
+    UITextField * nameTf = [[UITextField alloc] init];
+    nameTf.font = [UIFont systemFontOfSize:15];
+    nameTf.textColor = BlackColor;
+    nameTf.placeholder = @"请输入计数的标题";
+    [view addSubview:nameTf];
+    [nameTf mas_makeConstraints:^(MASConstraintMaker *make) {
         make.top.bottom.mas_equalTo(0);
         make.left.mas_equalTo(16);
         make.right.mas_equalTo(-16);
     }];
-
+    
+    UILabel * countLabel = [[UILabel alloc] init];
+    countLabel.text = @"目标";
+    countLabel.font = [UIFont systemFontOfSize:15];
+    [self.view addSubview:countLabel];
+    [countLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(view.mas_bottom).offset(16);
+        make.left.mas_equalTo(16);
+        make.right.mas_equalTo(-16);
+    }];
+    
+    UIView * viewCount = [[UIView alloc] init];
+    viewCount.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:viewCount];
+    [viewCount mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(countLabel.mas_bottom).offset(5);
+        make.left.right.mas_equalTo(0);
+        make.height.mas_equalTo(44);
+    }];
+    
+    countTf = [[UITextField alloc] init];
+    countTf.font = [UIFont systemFontOfSize:15];
+    countTf.textColor = BlackColor;
+    countTf.placeholder = @"请输入目标值";
+    countTf.keyboardType = UIKeyboardTypeNumberPad;
+    [viewCount addSubview:countTf];
+    [countTf mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.bottom.mas_equalTo(0);
+        make.left.mas_equalTo(16);
+    }];
+    
+    UIView * btnsView = [[UIView alloc] init];
+    [StaticTools setRoundView:btnsView radius:4 borderWidth:0.5 color:RGB(34, 209, 34)];
+    [viewCount addSubview:btnsView];
+    [btnsView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(10);
+        make.left.mas_equalTo(countTf.mas_right).offset(10);
+        make.right.mas_equalTo(-16);
+        make.bottom.mas_equalTo(-10);
+        make.width.mas_equalTo(101);
+    }];
+    
+    UIButton * reduceBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [reduceBtn addTarget:self action:@selector(changeCount:) forControlEvents:UIControlEventTouchUpInside];
+    [btnsView addSubview:reduceBtn];
+    reduceBtn.tag = 0;
+    [reduceBtn setTitle:@"-" forState:UIControlStateNormal];
+    [reduceBtn setTitleColor:RGB(34, 209, 34) forState:UIControlStateNormal];
+    [reduceBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.top.bottom.mas_equalTo(0);
+        make.width.mas_equalTo(btnsView.mas_width).multipliedBy(0.5);
+    }];
+    UIButton * addBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [addBtn addTarget:self action:@selector(changeCount:) forControlEvents:UIControlEventTouchUpInside];
+    [btnsView addSubview:addBtn];
+    addBtn.tag = 1;
+    [addBtn setTitle:@"+" forState:UIControlStateNormal];
+    [addBtn setTitleColor:RGB(34, 209, 34) forState:UIControlStateNormal];
+    [addBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.top.bottom.mas_equalTo(0);
+        make.width.mas_equalTo(btnsView.mas_width).multipliedBy(0.5);
+    }];
+    UIView * line = [[UIView alloc] init];
+    line.backgroundColor = RGB(34, 209, 34);
+    [btnsView addSubview:line];
+    [line mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.bottom.mas_equalTo(0);
+        make.center.mas_equalTo(btnsView.center);
+        make.width.mas_equalTo(0.5);
+    }];
 }
-
+-(void)changeCount:(UIButton *)btn
+{
+    if (btn.tag == 0 && countTf.text.integerValue == 0) return;
+    NSInteger count = countTf.text.integerValue;
+    count = btn.tag == 0 ? count - 1 : count + 1;
+    countTf.text = [NSString stringWithFormat:@"%ld",(long)count];
+}
 -(void)backAction:(UIButton *)btn
 {
     [self.navigationController popViewControllerAnimated:YES];
 }
 -(void)saveAction:(UIButton *)btn
 {
+    
 }
 -(void)viewWillAppear:(BOOL)animated
 {
